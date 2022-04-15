9c4de683053e12a3fe519342bab24dfed6ad6587
VASUDEV KANUNGO <77634422+vasu2705@users.noreply.github.com>
2022-04-15 12:52:10 +0530

    commit

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 const api = require("./api/2.json");
 app.use(express.json());
-const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   console.log(JSON.stringify(api));
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`the application started successfully on port ${port}`);
 });
